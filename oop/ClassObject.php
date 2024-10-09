84f6d983d238955280e30a386415ffe36b2b1ab3
HUDA26122022 <muliahudaahmad@gmail.com>
2024-10-09 21:50:36 +0700

    vidio 2 # Class & Object

---
+++ oop/ClassObject.php
@@ -0,0 +1,14 @@
+<?php 
+class ClassObject{ // nama kelas
+
+}
+
+//membuat sebuah object
+$a = new ClassObject();
+$b = new ClassObject();
+$c = new ClassObject();
+
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
---
+++ oop/produk.php
@@ -0,0 +1,15 @@
+<?php 
+
+//jualan produk
+//komik
+//Game
+
+class produk{
+
+}
+
+$produk1 = new produk();
+$produk2 = new produk();
+$produk3 = new produk();
+
+?>
