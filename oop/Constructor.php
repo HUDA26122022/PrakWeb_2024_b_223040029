b595efd5119bf1f3eb8bf14aedd5780db0d7fc7c
HUDA26122022 <muliahudaahmad@gmail.com>
2024-10-13 17:06:45 +0700

    Vidio 5 # Object type

--- oop/Constructor.php
+++ oop/Constructor.php
@@ -16,21 +16,37 @@ class produk{
          $this->harga = $harga;
     }
     
-    public function getLabel(){
+    public function getLabel(){ //method
         return "$this->judul, Rp $this->harga";
     }
 }
+
+
+//mencetak seluruh informasi produk
+class CetakInfoBuku {
+    public function cetak ( Produk $produk ){
+        $str = "{$produk -> judul} | {$produk-> getLabel()} (Rp. {$produk -> harga}) ";
+        return $str;
+    }
+
+}
+
+
 
 //intsansiasi
 
 $produk1 = new produk("Naruto", "Ahmad", "Gramedia", 30000);//komik
 $produk2 = new produk("Minecraft", "Mojang", "Mojang", 99000);//game
-$produk3 = new produk("Gantungan Makrame", "Ahmad Mulia Huda", "AmhStore.id", 10000);
+
 
 //menampilkan kelayar 
 echo "Komik :" . $produk1->getLabel();
 echo "<br>"; //garis baru
 echo "Game :" . $produk2->getLabel();
+
 echo "<br>";
-echo "produk :" . $produk3->getLabel();
+
+$infoProduk1 = new CetakInfoBuku();
+echo $infoProduk1->cetak($produk1);
+
 ?>
