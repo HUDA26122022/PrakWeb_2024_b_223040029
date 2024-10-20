e355d54f9996cca81290802a67daed61fe8c39cc
HUDA26122022 <muliahudaahmad@gmail.com>
2024-10-20 22:54:24 +0700

    vidio 7 # Model

---
+++ mvc/app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php 
+
+class User_model{
+    private $nama = 'Ahmad Mulia Huda';
+
+    public function getUser()
+    {
+     return $this->nama;
+    }
+}
--- mvc/app/views/Home/index.php
+++ mvc/app/views/Home/index.php
@@ -1,7 +1,7 @@
 <div class ="container">
     <div class = "jumbotron mt-4">
         <h1 class = "display-4">Kamu berada di halaman home saya</h1>
-        <p class = "lead"> nama saya ahmad mulia huda dengan nrp 223040029</p>
+        <p class = "lead"> nama saya  <?= $data['nama']; ?> dengan nrp 223040029</p>
         <hr class = "my-4">
         <p> saya berasah dari daerah tasikmalaya</p>
         <a class = "btn btn-primary btn-lg" href="" role="button"> Learn more</a>
