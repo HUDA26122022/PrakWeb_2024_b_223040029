d831960d3420abca22ae4d6bdd849ea99666933d
HUDA26122022 <muliahudaahmad@gmail.com>
2024-10-26 06:30:20 +0700

    vidio 11 #delete data

--- mvc/app/models/Mahasiswa_model.php
+++ mvc/app/models/Mahasiswa_model.php
@@ -21,7 +21,7 @@ class Mahasiswa_model{
         $this->db->bind('id', $id);
         return $this->db->single();
     }
-    
+
     public function tambahDataMahasiswa($data)
     {
         $query = "INSERT INTO mahasiswa (nama, nrp, email, jurusan) 
@@ -32,6 +32,15 @@ class Mahasiswa_model{
         $this->db->bind('nrp', $data['nrp']);
         $this->db->bind('email', $data['email']);
         $this->db->bind('jurusan', $data['jurusan']);
+        $this->db->execute();
+        return $this->db->rowCount();
+    }
+
+    public function hapusDataMahasiswa($id)
+    {
+        $query = "DELETE FROM mahasiswa WHERE id = :id";
+        $this->db->query($query);
+        $this->db->bind('id', $id);
         $this->db->execute();
         return $this->db->rowCount();
     }
